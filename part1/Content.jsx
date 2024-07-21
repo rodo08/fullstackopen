7e289d147e08b1a5e40353eab8afa5f7ea28f054
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-07-21 08:59:40 +0200

    Add ex 1.1

---
+++ part1/Content.jsx
@@ -0,0 +1,11 @@
+const Content = ({ partName, numberOfExcercises }) => {
+  return (
+    <div>
+      <p>
+        {partName} has {numberOfExcercises} excercises.
+      </p>
+    </div>
+  );
+};
+
+export default Content;
